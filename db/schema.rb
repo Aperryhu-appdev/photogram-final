085e45cbfdc72557f81d57eafaca85fb966eb227
Aperryhu <aperryhu@chicagobooth.edu>
2022-10-08 21:59:17 +0000

    Migrated Comment Table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_08_214739) do
+ActiveRecord::Schema.define(version: 2022_10_08_215126) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.integer "author_id"
+    t.integer "photo_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "photos", force: :cascade do |t|
     t.text "caption"
